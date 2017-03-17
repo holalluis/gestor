f6dae87fa3e9a7e9bccccbf975f31392ba0a89f1
lbosch <lbosch@icra.cat>
2017-03-17 16:44:51 +0100

    canvi de POST per GET

--- data/insert.php
+++ data/insert.php
@@ -1,11 +1,11 @@
 <?php
 
 	//check input
-	if(!isset($_POST['par'],$_POST['val']))
+	if(!isset($_GET['param'],$_GET['value']))
 	{
 		$url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 		die("<b>Error!</b><br>
-			$url &rarr; variables POST: par,val<br>
+			$url?param=xxx&value=xxx
 			<ul>
 				<li>par: nom paràmetre 
 				<li>val: valor paràmetre
@@ -14,17 +14,17 @@
 	}
 
 	//input
-	$par=$_POST=['par'];
-	$val=$_POST=['val'];
+	$param=$_GET['param'];
+	$value=$_GET['value'];
 
 	//comprova si estan en blanc
-	if($par==""){die("El paràmetre 'par' està en blanc");}
-	if($val==""){die("El paràmetre 'val' està en blanc");}
+	if($param==""){die("El paràmetre 'param' està en blanc");}
+	if($value==""){die("El paràmetre 'value' està en blanc");}
 
 	//resultat
 	echo "
-		Has enviat correctament els valors $par:$val
-		<br>
+		Has enviat correctament els valors '$param'=$value
+		<br> <br>
 		En un futur els guardarem a la base de dades
 	";
 
